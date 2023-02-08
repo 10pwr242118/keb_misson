d6ce5a9ebe262ac0709f9284528f0fbb5fd4b09c
jade <appeloset@gmail.com>
2023-02-08 22:45:28 +0900

    chap09, 01, 0208

---
+++ mission_chap09_01.py
@@ -0,0 +1,103 @@
+class Graph():
+    def __init__(self, graph_size) :
+        self.size = graph_size
+        self.graph = [[0 for _ in range(graph_size)]for _ in range(graph_size)]
+
+def graph_generator_a(graph_str):
+    splited_list = graph_str.split()
+    edge_list= []
+    for i in splited_list: edge_list.extend(i.split("-"))
+    graph_size = int(sorted(edge_list, reverse=True)[0]) + 1
+    graph = Graph(graph_size)
+    for row, col in edge_list:
+        graph.graph[row][col] = 1
+        graph.graph[col][row] = 1
+    return graph
+
+
+def graph_generator(edge_str):
+    global store_array
+    splited_list = edge_str.split()
+    # print(splited_list)
+    edge_list= []
+    for i in splited_list: edge_list.extend(list(map(int, i.split("-"))))
+    # print(edge_list)
+    graph_size = int(sorted(edge_list, reverse=True)[0]) + 1
+    # print(graph_size)
+    graph = Graph(graph_size)
+    for i in range(len(edge_list)):
+        if i % 2 == 0:
+            a = edge_list[i]
+            b = edge_list[i+1]
+            graph.graph[a][b] = 1
+            graph.graph[b][a] = 1
+    print("graph is generate")
+    print_graph(graph)
+    return graph
+
+
+def print_graph(g) :
+    global store_array
+    print('	', end='')
+    for v in range(g.size) :
+        print("%9s" % store_array[v][0], end = ' ')
+    print()
+    for row in range(g.size) :
+        print("%9s" %store_array[row][0], end = ' ')
+        for col in range(g.size) :
+            print("%8d" % g.graph[row][col], end = ' ')
+        print()
+    print()
+
+
+def graph_put_data(graph):
+    global store_array
+    for idx in range(graph.size):
+        graph.graph[idx][idx] = store_array[idx][1]
+    print("data is store in data")
+    print_graph(graph)
+    return graph
+
+
+def find_max_stock(g):
+    global store_array
+
+    stack = []
+    visitedAry = []
+    stock_temp= []
+
+    # find init
+    current = 0
+    stack.append(current)
+    visitedAry.append(current)
+    stock_temp.append([current ,g.graph[current][current]])
+
+    while (len(stack) != 0) :
+        next = None
+        for vertex in range(g.size-1) :
+            if g.graph[current][vertex+1] == 1 :
+                if vertex+1 in visitedAry :	# 방문한 적이 있는 편의점이면 탈락
+                    pass
+                else :			# 방문한 적이 없는 편의점이면 다음 편의점으로 지정
+                    next = vertex+1
+                    break
+
+        if next != None :				# 방문할 다음 편의점이 있는 경우
+            current = next
+            stack.append(current)
+            visitedAry.append(current)
+            stock_temp.append([current ,g.graph[current][current]])
+        else :
+            current = stack.pop()
+    # print(stock_temp)
+    sorted_temp = sorted(stock_temp ,key=lambda x :x[1] , reverse=True)
+    print(f"{store_array[sorted_temp[0][0]][0]} has most honey-butter chip. Stock is {sorted_temp[0][1]}")
+
+if __name__ == '__main__':
+    str= "0-1 0-2 1-2 1-3 2-3 3-4"
+    store_array = [['GS25', 30], ['CU', 60], ['Seven11', 10], ['MiniStop', 90], ['Emart24', 40]]
+
+    graph_one=graph_generator(str)
+    graph_one= graph_put_data(graph_one)
+    find_max_stock(graph_one)
+
